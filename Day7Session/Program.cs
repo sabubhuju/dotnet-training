5833b6d0df0704668d05ba6b856a0aedb552ec6b
sabubhuju <sabubhuju0613@gmail.com>
2026-04-17 08:17:04 +0545

    Updated new added

---
+++ Day7Session/Program.cs
@@ -0,0 +1,26 @@
+﻿/*
+1.Collections: array, list, dictionary, hashset, queue, stack
+2.LINQ
+3.File Operations
+*/
+
+/*
+IEnumerable and IEnumerator
+*/
+
+//Array Examples
+class ArrayExample
+{
+    static void Main()
+    {
+        //1D Array
+        int[] scores = new int[5];
+        scores[0] = 95;
+        scores[1] = 88;
+        scores[2] = 78;
+
+        Console.WriteLine(scores[1]);
+
+
+    }
+}
